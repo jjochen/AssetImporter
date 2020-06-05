9386815bbded5be1fdcdaccf5bea53c81447c10e
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2020-06-05 21:56:06 +0200

    Initial Commit

---
+++ assets-import/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  assets-import
+//
+//  Created by Jochen on 05.06.20.
+//  Copyright © 2020 Jochen Pfeiffer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
