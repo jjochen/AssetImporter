312e22fe1ee995188635d9f12a8d9bf47b3dcea9
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2020-08-09 21:25:24 +0200

    add more tests

--- Tests/AssetImporterTests/CommandLineTaskTests.swift
+++ Tests/AssetImporterTests/CommandLineTaskTests.swift
@@ -43,6 +43,34 @@ final class CommandLineTaskTests: XCTestCase {
         }
     }
 
+    func testRoundedSVGIsEqualRoundedPDF() {
+        do {
+            let svgPath1 = try testFolder.filePath(forResource: .add16ptRoundedSVG)
+            let pdfPath1 = testFolder.filePathInWorkFolder(forFileNamed: "test.pdf")
+            let pdfPath2 = try testFolder.filePath(forResource: .add16ptRoundedPDF)
+            CommandLineTask.scaleSVG(at: svgPath1, destination: pdfPath1, scale: 0.5)
+            let isEqual = CommandLineTask.image(at: pdfPath1, isEqualToImageAt: pdfPath2)
+            XCTAssert(isEqual)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
+    func testRoundedSVGIsNotEqualNoneRoundedSVG() {
+        do {
+            let svgPath1 = try testFolder.filePath(forResource: .add16ptSVG)
+            let svgPath2 = try testFolder.filePath(forResource: .add16ptRoundedSVG)
+            let pdfPath1 = testFolder.filePathInWorkFolder(forFileNamed: "not_rounded.pdf")
+            let pdfPath2 = testFolder.filePathInWorkFolder(forFileNamed: "rounded.pdf")
+            CommandLineTask.scaleSVG(at: svgPath1, destination: pdfPath1, scale: 0.5)
+            CommandLineTask.scaleSVG(at: svgPath2, destination: pdfPath2, scale: 0.5)
+            let isEqual = CommandLineTask.image(at: pdfPath1, isEqualToImageAt: pdfPath2)
+            XCTAssertFalse(isEqual)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
     static var allTests = [
         ("testResourcesReadable", testResourcesReadable),
         ("testScaleSVGTask", testScaleSVGTask),
--- Tests/AssetImporterTests/TestFolder.swift
+++ Tests/AssetImporterTests/TestFolder.swift
@@ -39,7 +39,7 @@ struct Resource {
     static var add16ptRoundedSVG: Resource {
         let fileName = "add_16pt_rounded.svg"
         let size = CGSize(width: 16, height: 16)
-        let svgString = "<svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32'><title>c-add</title><g stroke-linecap='square' stroke-linejoin='miter' stroke-width='2' fill='#444444' stroke='#444444'><line fill='none' stroke-miterlimit='10' x1='16' y1='9' x2='16' y2='23'></line> <line fill='none' stroke-miterlimit='10' x1='23' y1='16' x2='9' y2='16'></line> <circle fill='none' stroke='#444444' stroke-miterlimit='10' cx='16' cy='16' r='15'></circle></g></svg>"
+        let svgString = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32'><title>add_16pt</title><g class='nc-icon-wrapper' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' fill='#444444' stroke='#444444'><line data-color='color-2' fill='none' stroke-miterlimit='10' x1='16' y1='9' x2='16' y2='23'/> <line data-color='color-2' fill='none' stroke-miterlimit='10' x1='23' y1='16' x2='9' y2='16'/> <circle fill='none' stroke='#444444' stroke-miterlimit='10' cx='16' cy='16' r='15'/></g></svg>"
         return Resource(fileName: fileName, size: size, svgString: svgString)
     }
 
